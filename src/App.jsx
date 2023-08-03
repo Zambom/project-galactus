f8f10e7ce647fdb9e6c007185bf88921b387232f
Zambom <egz@cin.ufpe.br>
2023-08-03 09:05:46 -0300

    feat: initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <mesh>
+        <torusKnotGeometry />
+        <meshNormalMaterial />
+      </mesh>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Canvas } from '@react-three/fiber'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Canvas>
+    <App />
+  </Canvas>,
+)
