1ea01a7b9d966bdf13e4d6cd2627f9edbe0d1bec
Zambom <erikzambom@hotmail.com>
2023-08-08 18:00:43 -0300

    feat: adding gassy planet

--- src/components/Planets/main.jsx
+++ src/components/Planets/main.jsx
@@ -2,7 +2,7 @@ import { useFrame } from "@react-three/fiber"
 import { Vector3 } from "three"
 import { useLayoutEffect, useMemo, useRef } from "react"
 
-import vertexShader from "../../shaders/Planet/rocky/vertex.glsl"
+import vertexShader from "../../shaders/Planet/gassy/vertex.glsl"
 import fragmentShader from "../../shaders/Planet/gassy/fragment.glsl"
 
 export default function Planet() {
